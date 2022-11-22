7e169c384cdee045931670f6a835ffc4a2b4bae8
vishwajith <vishwakr1998@gmail.com>
2022-11-22 16:52:29 +0530

    finished

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './Sidebar';
+import Topbar from './Topbar';
+import Dashboard from './Dashboard';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Studentlist from './Studentlist';
+import Attendancelist from './Attendancelist';
+import Addattendance from './Addattendance';
+
+function App() {
+  return (
+    <div id="wrapper">
+      <BrowserRouter>
+
+        <Sidebar></Sidebar>
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Topbar></Topbar>
+            <div class="container-fluid">
+
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/studentlist" element={<Studentlist />} />
+                <Route path="/student/:id" element={<Attendancelist />} />
+                <Route path="/add-attendance/:id" element={<Addattendance />} />
+
+
+              </Routes>
+            </div>
+          </div>
+
+        </div>
+
+      </BrowserRouter>
+    </div>
+
+
+
+  );
+}
+
+export default App;
---
+++ src/Studentlist.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+
+function Studentlist() {
+
+    const [Students, setStudents] = useState([])
+
+
+    let Name = async () => {
+        try {
+            let StudentData = await fetch("https://637398230bb6b698b610942d.mockapi.io/student")
+            let Studentlist = await StudentData.json();
+            setStudents(Studentlist);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        Name()
+    }, []);
+
+
+
+    return (
+        <>
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Student list</h1>
+                <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                    class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+            </div>
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Avatar</th>
+                        <th>Action</th>
+
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Avatar</th>
+                        <th>Action</th>
+
+
+                    </tr>
+                </tfoot>
+                <tbody>
+
+
+                    {
+                        Students.map((student) => {
+                            return (
+                                <tr>
+                                    <td>{student.id}</td>
+                                    <td>{student.name}</td>
+                                    <td>
+                                        <img src={student.avatar} />
+                                    </td>
+                                    <td>
+                                        <Link to={`/student/${student.id}`}>
+                                            <button className='btn btn-primary'>View</button>
+                                        </Link>
+
+
+
+                                    </td>
+                                </tr>
+
+                            )
+                        })
+                    }
+
+
+
+
+
+                </tbody>
+            </table>
+
+        </>
+    )
+}
+
+export default Studentlist
